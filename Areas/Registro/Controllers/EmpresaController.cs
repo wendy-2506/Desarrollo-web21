e47bd9510a2b1124de6474125452f49faaf8dda6
Leonardo Michael Villacrez Ortiz <maycol4566@hotmail.com>
2021-06-27 17:52:19 -0500

    Creación Registro

---
+++ Areas/Registro/Controllers/EmpresaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tu_Nuevo_Trabajo2021.Areas.Registro.Controllers
+{
+    [Area("Registro")]
+    public class EmpresaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
