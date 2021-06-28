746015367456acc6e976527593c847bad7b9b7fa
Leonardo Michael Villacrez Ortiz <maycol4566@hotmail.com>
2021-06-27 22:09:52 -0500

    Login_V2

---
+++ Areas/LOGIN2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tu_Nuevo_Trabajo2021.Areas.LOGIN2.Controllers
+{
+    [Area("LOGIN2")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
