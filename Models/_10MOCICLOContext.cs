42b5a82f227c9e910a53ceefd5d8819a2029e54c
Wendy Mendoza Pérez <wendy_25_06@outlook.com>
2021-07-09 21:43:21 -0500

    todo admin con vistas

--- Models/_10MOCICLOContext.cs
+++ Models/_10MOCICLOContext.cs
@@ -31,7 +31,7 @@ namespace Tu_Nuevo_Trabajo2021.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-U2KSAMR;database=10MOCICLO;MultipleActiveResultSets=true;Trusted_Connection=true");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-TDVG9EP;database=10MOCICLO;MultipleActiveResultSets=true;Trusted_Connection=true");
             }
         }
 
